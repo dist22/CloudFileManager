247c7d56550612bdba006b73154623e0457a75b8
Di_St <dima.stozhok335@gmail.com>
2025-06-26 23:21:44 +0300

    Added middleware

---
+++ Cloud/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using Cloud.Exceptions;
+
+namespace Cloud.Middleware;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+{
+    public async Task Invoke(HttpContext context)
+    {
+
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            
+            context.Response.ContentType = "application/json";
+
+            context.Response.StatusCode = ex switch
+            {
+                NotFoundException => StatusCodes.Status404NotFound,
+                ForbiddenException => StatusCodes.Status403Forbidden,
+                ConflictException => StatusCodes.Status409Conflict,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            var response = new
+            {
+                error = ex.Message,
+                status = context.Response.StatusCode,
+            };
+            
+            var json = JsonSerializer.Serialize(response);
+            await context.Response.WriteAsync(json);
+
+        }
+
+    }
+
+}
