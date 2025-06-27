6589945e4df16af9af5ded55af2ce5ee001e1250
Di_St <dima.stozhok335@gmail.com>
2025-06-27 20:09:00 +0300

    Deleted user and file repo, now used only baseRepo

--- Cloud/Services/AuthServices.cs
+++ Cloud/Services/AuthServices.cs
@@ -9,22 +9,22 @@ using Cloud.Models;
 
 namespace Cloud.Services;
 
-public class AuthServices : IAuthServices
+public class AuthServices : BaseServices, IAuthServices
 {
-    private readonly IUserRepository _userRepository;
+    private readonly IBaseRepository<User> _userRepository;
     private readonly IBlobStorage _blobStorage;
     private readonly IMapper _mapper;
     private readonly IPasswordHasher _passwordHasher;
     private readonly IJwtProvider _jwtProvider;
 
-    public AuthServices(IUserRepository userRepository, IBlobStorage blobStorage, IMapper mapper,
-        IPasswordHasher passwordHasher, IJwtProvider jwtProvider)
+    public AuthServices(IBlobStorage blobStorage, IMapper mapper, IPasswordHasher passwordHasher,
+        IJwtProvider jwtProvider, IBaseRepository<User> userRepository)
     {
-        _userRepository = userRepository;
         _blobStorage = blobStorage;
         _mapper = mapper;
         _passwordHasher = passwordHasher;
         _jwtProvider = jwtProvider;
+        _userRepository = userRepository;
     }
 
 
@@ -34,20 +34,21 @@ public class AuthServices : IAuthServices
         {
             if (await PasswordConfirmAsync(userCreateDto.password, userCreateDto.passwordConfirm))
             {
-                var user = await _userRepository.AddUserAsync((_mapper.Map<User>(userCreateDto)));
+                var user = await _userRepository.AddAsync(_mapper.Map<User>(userCreateDto));
                 user.containerName = await _blobStorage.CreateUserContainerAsync(user.userId.ToString());
                 user.password = _passwordHasher.Generate(userCreateDto.password);
-                return await _userRepository.EditUser(user);
+                return await _userRepository.Update(user);
             }
 
             throw new Exception("Password don`t match");
         }
+
         throw new Exception("User already exists");
     }
 
     public async Task<string> LoginAsync(UserLoginDTO userLoginDto)
     {
-        var user = await _userRepository.GetUserIfExistAsync(u => u.email == userLoginDto.email);
+        var user = await GetIfNotNullAsync(_userRepository.GetAsync(u => u.email == userLoginDto.email));
         if (_passwordHasher.Verify(userLoginDto.password, user.password))
             return _jwtProvider.CreateToken(user);
         throw new Exception("error");
@@ -57,6 +58,6 @@ public class AuthServices : IAuthServices
         => Task.FromResult(password == passwordConfirm);
 
     public async Task<bool> IfExistAsync(string email, string username)
-        => await _userRepository.UserExists(u => u.email == email) &&
-           await _userRepository.UserExists(u => u.email == username);
+        => await _userRepository.IfExistAsync(u => u.email == email) &&
+           await _userRepository.IfExistAsync(u => u.email == username);
 }
--- Cloud/Services/FileServices.cs
+++ Cloud/Services/FileServices.cs
@@ -8,31 +8,33 @@ using Cloud.Interfaces.Services;
 
 namespace Cloud.Services;
 
-public class FileServices : IFileServices
+public class FileServices : BaseServices, IFileServices
 {
-    private readonly IUserRepository _userRepository;
-    private readonly IFileRepository _fileRepository;
+    private readonly IBaseRepository<FileRecord> _fileRepository;
+    private readonly IBaseRepository<User> _userRepository;
     private readonly IBlobStorage _blobStorage;
     private readonly IFileSizeConverter _fileSizeConverter;
     private readonly IMapper _mapper;
 
-    public FileServices(IUserRepository userRepository, IFileRepository fileRepository, IMapper mapper,
-        IBlobStorage blobStorage, IFileSizeConverter fileSizeConverter)
+    public FileServices(IMapper mapper, IBlobStorage blobStorage, IFileSizeConverter fileSizeConverter,
+        IBaseRepository<FileRecord> fileRepository, IBaseRepository<User> userRepository)
     {
         _mapper = mapper;
         _blobStorage = blobStorage;
         _fileSizeConverter = fileSizeConverter;
+        _fileRepository = fileRepository;
+        _userRepository = userRepository;
     }
 
 
     public async Task<string> UploadFileAsync(int userId, IFormFile file)
     {
-        var user = await _userRepository.GetUserIfExistAsync(u => u.userId == userId);
+        var user = await GetIfNotNullAsync(_userRepository.GetAsync(u => u.userId == userId));
 
         var originalFileName = Path.GetFileNameWithoutExtension(file.FileName);
         var extension = Path.GetExtension(file.FileName);
         var uniqueFileName = $"{originalFileName}_{Guid.NewGuid()}{extension}";
-        
+
         var fileRecord = new FileRecord
         {
             fileName = uniqueFileName,
@@ -42,40 +44,40 @@ public class FileServices : IFileServices
             userId = user.userId
         };
 
-        await _fileRepository.AddFileAsync(fileRecord);
+        await _fileRepository.AddAsync(fileRecord);
         user.files.Add(fileRecord);
-        await _userRepository.EditUser(user);
+        await _userRepository.Update(user);
 
         return fileRecord.filePath;
     }
 
     public async Task<bool> DeleteAnyFileAsync(int fileId)
     {
-        var file = await _fileRepository.GetFileIfExistAsync(fileId);
-        var user = await _userRepository.GetUserIfExistAsync(u => u.userId == file.userId);
-        
-        return await _blobStorage.DeleteAsync(file, user) 
-               && await _fileRepository.DeleteFileAsync(file);
+        var file = await GetIfNotNullAsync(_fileRepository.GetAsync(f => f.fileId == fileId));
+        var user = await GetIfNotNullAsync(_userRepository.GetAsync(u => u.userId == file.userId));
+
+        return await _blobStorage.DeleteAsync(file, user)
+               && await _fileRepository.Remove(file);
     }
 
     public async Task<bool> DeleteUserFileAsync(int userId, int fileId)
     {
-       var file = await _fileRepository.GetFileIfExistAsync(fileId);
-       var user = await _userRepository.GetUserIfExistAsync(u => u.userId == userId);
-       
-       if (file.userId != userId)
-           return false;
-       
-       return await _fileRepository.DeleteFileAsync(file) 
-              && await _blobStorage.DeleteAsync(file, user);
+        var file = await GetIfNotNullAsync(_fileRepository.GetAsync(f => f.fileId == fileId));
+        var user = await GetIfNotNullAsync(_userRepository.GetAsync(u => u.userId == userId));
+
+        if (file.userId != userId)
+            return false;
+
+        return await _fileRepository.Remove(file)
+               && await _blobStorage.DeleteAsync(file, user);
     }
 
-    public async Task<FileDTOs> GetFileByIdAsync(int fileId) 
-        => _mapper.Map<FileDTOs>(await _fileRepository.GetFileIfExistAsync(fileId));
+    public async Task<FileDTOs> GetFileByIdAsync(int fileId)
+        => _mapper.Map<FileDTOs>(await GetIfNotNullAsync(_fileRepository.GetAsync(f => f.fileId == fileId)));
 
     public async Task<FileDTOs> GetMyFileAsync(int fileId, int fileOwnerId)
     {
-        var file = await _fileRepository.GetFileIfExistAsync(fileId);
+        var file = await GetIfNotNullAsync(_fileRepository.GetAsync(f => f.fileId == fileId));
         if (file.userId != fileOwnerId)
             throw new UnauthorizedAccessException();
         return _mapper.Map<FileDTOs>(file);
@@ -84,30 +86,30 @@ public class FileServices : IFileServices
 
     public async Task<IEnumerable<FileDTOs>> GetAllUserFilesAsync(int userId)
     {
-        var files = await _fileRepository.GetFilesAsync();
+        var files = await _fileRepository.GetAllAsync();
         var result = files.Where(f => f.userId == userId).ToList();
         return _mapper.Map<IEnumerable<FileDTOs>>(result);
     }
 
     public async Task<IEnumerable<FileDTOs>> GetAllFilesAsync()
     {
-        var result = await _fileRepository.GetFilesAsync();
+        var result = await _fileRepository.GetAllAsync();
         return _mapper.Map<IEnumerable<FileDTOs>>(result);
     }
 
     public async Task<(Stream?, string fileName)?> DownloadFileAsync(int fileId)
     {
-        var file = await _fileRepository.GetFileIfExistAsync(fileId);
-        var user = await _userRepository.GetUserIfExistAsync(u => u.userId == file.userId);
-        
+        var file = await GetIfNotNullAsync(_fileRepository.GetAsync(f => f.fileId == fileId));
+        var user = await GetIfNotNullAsync(_userRepository.GetAsync(u => u.userId == file.userId));
+
         return (await _blobStorage.DownloadAsync(file, user), file.fileName);
     }
 
     public async Task<(Stream?, string fileName)?> DownloadMyFileAsync(int userId, int fileId)
     {
-        var file = await _fileRepository.GetFileIfExistAsync(fileId);
-        var user = await _userRepository.GetUserIfExistAsync(u => u.userId == userId);
-      
+        var file = await GetIfNotNullAsync(_fileRepository.GetAsync(f => f.fileId == fileId));
+        var user = await GetIfNotNullAsync(_userRepository.GetAsync(u => u.userId == userId));
+
         if (file.userId != userId)
             throw new UnauthorizedAccessException();
 
