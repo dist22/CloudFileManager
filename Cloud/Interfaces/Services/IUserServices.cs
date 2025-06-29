90f8e4babb1ae17b4339f4c9195df6a538a9e6cd
Di_St <dima.stozhok335@gmail.com>
2025-06-29 16:59:48 +0300

    No return value. Method throw expections inside

--- Cloud/Interfaces/Services/IUserServices.cs
+++ Cloud/Interfaces/Services/IUserServices.cs
@@ -7,7 +7,7 @@ public interface IUserServices
 {
     public Task<IEnumerable<UserDTOs>> GetUsersAsync();
     public Task<UserDTOs> GetUserAsync(int userId);
-    public Task<bool> EditUserAsync(int userId, string role);
-    public Task<bool> DeleteUserAsync(int userId);
+    public Task EditUserAsync(int userId, string role);
+    public Task DeleteUserAsync(int userId);
     
 }
